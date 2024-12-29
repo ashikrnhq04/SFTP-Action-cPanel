0a093328d7ac837dddb06902374653eda7ff8eaa
Ashikur Rahman <ashikrn.hq04@gmail.com>
2024-12-29 19:50:46 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,43 @@
 const core = require('@actions/core');
-const SftpClient = require('ssh2-sftp-client');
-const path = require('path');
+const ftp = require('basic-ftp');
 
 async function run() {
-  const sftp = new SftpClient();
+  const client = new ftp.Client();
+  client.ftp.verbose = true;
+
   try {
     // Get inputs
     const host = core.getInput('host');
-    const port = core.getInput('port') || 22; 
+    const port = core.getInput('port') || 21;
     const username = core.getInput('username');
     const password = core.getInput('password');
     const source = core.getInput('source') || './';
     const target = core.getInput('target');
 
+    if (!host || !username || !password || !target) {
+      core.setFailed('Required inputs (host, username, password, target) are missing.');
+      return;
+    }
 
     // Connect to the server
-    await sftp.connect({
-      host: host,
-      username: username,
-      password: password,
-      port: parseInt(port, 10), 
+    core.info(`Connecting to ${host}:${port}`);
+    await client.access({
+      host,
+      port: parseInt(port, 10),
+      user: username,
+      password,
+      secure: false, // Set to 'true' if FTPS is required
     });
 
-
     // Upload the local folder to the remote folder
-    await sftp.uploadDir(source, target);
+    core.info(`Uploading files from ${source} to ${target}...`);
+    await client.uploadFromDir(source, target);
     core.info(`Files uploaded successfully from ${source} to ${target}`);
 
   } catch (err) {
     core.setFailed(`Action failed: ${err.message}`);
   } finally {
-    sftp.end();
+    client.close();
   }
 }
 run();
